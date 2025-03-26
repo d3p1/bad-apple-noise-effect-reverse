f3199a4a963ef077361d59eb15b19045b2ddbaa3
C. M. de Picciotto <d3p1@d3p1.dev>
2025-03-26 01:38:37 +0000

    feat: init effect [#1]

--- src/js/app.ts
+++ src/js/app.ts
@@ -2,7 +2,7 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import badAppleVideo from '../media/videos/bad-apple.webm'
+import badAppleVideo from '../media/videos/bad-apple.mp4'
 
 class App {
   /**
@@ -19,6 +19,11 @@ class App {
    * @type {CanvasRenderingContext2D}
    */
   #context: CanvasRenderingContext2D
+
+  /**
+   * @type {ImageData | null}
+   */
+  #beforeFrameData: ImageData | null = null
 
   /**
    * Constructor
@@ -51,7 +56,37 @@ class App {
 
     this.#draw()
 
+    this.#postProcessing()
+
     requestAnimationFrame(this.#run.bind(this))
+  }
+
+  /**
+   * Post-processing
+   *
+   * @returns {void}
+   */
+  #postProcessing(): void {
+    if (!this.#beforeFrameData) {
+      this.#beforeFrameData = this.#getImageData()
+    } else {
+      const currentFrameData = this.#getImageData()
+      for (let i = 0; i < currentFrameData.data.length; i += 4) {
+        if (currentFrameData.data[i] !== this.#beforeFrameData.data[i]) {
+          currentFrameData.data[i] = 0
+          currentFrameData.data[i + 1] = 0
+          currentFrameData.data[i + 2] = 0
+        } else {
+          currentFrameData.data[i] = 255
+          currentFrameData.data[i + 1] = 255
+          currentFrameData.data[i + 2] = 255
+        }
+      }
+
+      this.#beforeFrameData = this.#getImageData()
+
+      this.#context.putImageData(currentFrameData, 0, 0)
+    }
   }
 
   /**
@@ -102,10 +137,26 @@ class App {
    */
   #initCanvas(): void {
     this.#canvas = document.createElement('canvas')
-    this.#context = this.#canvas.getContext('2d') as CanvasRenderingContext2D
+    this.#context = this.#canvas.getContext('2d', {
+      willReadFrequently: true,
+    }) as CanvasRenderingContext2D
 
     this.#canvas.style.border = '1px solid #000'
     document.body.appendChild(this.#canvas)
+  }
+
+  /**
+   * Get image data
+   *
+   * @returns {ImageData}
+   */
+  #getImageData(): ImageData {
+    return this.#context.getImageData(
+      0,
+      0,
+      this.#canvas.width,
+      this.#canvas.height,
+    )
   }
 }
 new App()
